8fa0856b1bea2c318ed2289504a2b46c85e364f4
petrz <nhuthaotieu@gmail.com>
2026-01-19 14:05:13 +0700

    Add retry logic to mc command execution
    Introduced an attempt counter and retry mechanism for the mc command. If an error occurs, the command will retry up to the configured maximum attempts, with a timeout between retries. The embed footer indicates the attempt number on retries.

--- commands/mc.js
+++ commands/mc.js
@@ -15,7 +15,8 @@ module.exports = {
      * @param {ChatInputCommandInteraction} interaction 
      * @param {InteractionResponse} deferred
      */
-    async execute(interaction, deferred){
+    async execute(interaction, deferred, attempt=0){
+        attempt++;
         let color = interaction.guild?.members?.me?.displayHexColor || process.env.DEFAULT_COLOR;
         let embed = new EmbedBuilder().setColor(color);
       
@@ -25,17 +26,20 @@ module.exports = {
             .setDescription(getMotdText(response.description))
             .setThumbnail("attachment://icon.png")
             .addFields({name: "Status", value: response.version.protocol !== -1 ? "Online" : response.version.name.replaceAll(/§./g, "").replaceAll("●", "").replaceAll("◌", ""), inline: true});
-
             if(response.version.protocol !== -1) embed.addFields({name: "Version", value: response.version.name, inline: true});
-
             embed.addFields({name: "Players", value: `${response.players.online}/${response.players.max}`, inline: true});
-
             if(response.players?.sample?.length) embed.addFields({name: "Players online", value: `\`\`\`\n${getPlayerList(response.players.sample).slice(0, 1000)}\n\`\`\``});
+
+            if(attempt > 1) embed.setFooter({text: `Attempt ${attempt}`});
 
             const iconAttachment = new AttachmentBuilder(Buffer.from(response.favicon.split(",")[1], "base64"), {name: "icon.png"});
             deferred.edit({embeds: [embed], files: [iconAttachment]});
         }
         catch (error) {
+            if(attempt < parseInt(process.env.MINECRAFT_SERVER_MAX_ATTEMPT)) {
+                setTimeout(()=>this.execute(interaction, deferred, attempt), parseInt(process.env.MINECRAFT_SERVER_ATTEMPT_TIMEOUT));
+                return;
+            }
             deferred.edit({embeds: [embed.setDescription(`Encountered an error!\n\`\`\`\n${error?.toString()?.slice(0,2000)}\n\`\`\``)]});
         }
     },
